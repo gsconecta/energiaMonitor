7a4b3be36e1fdbcda4ed3c7864456cfb9de5df3b
gsconecta <contacto.gsconecta@gmail.com>
2025-10-01 08:49:19 +0200

    welcome

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,5 +1,4 @@
 import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
@@ -8,7 +7,7 @@ export default function AppLogo() {
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    Energía Monitor
                 </span>
             </div>
         </>
