8d9701f1b779583e4efed1bac5b1d726e665bce7
GS Coneta <contacto@gsconecta.es>
2026-03-11 09:59:07 +0100

    feat: Add Spanish language files for authentication, pagination, and password reset.

---
+++ lang/es/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Idioma de Paginación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son utilizadas por la biblioteca de paginador
+    | para construir los enlaces de paginación simples. Eres libre de cambiarlos
+    | a cualquier otra cosa que desees para personalizar tus vistas.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+
+];
---
+++ lang/es/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Idioma de Restablecimiento de Contraseña
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las predeterminadas que coinciden con
+    | las razones que da el gestor de contraseñas por las que ha fallado
+    | un intento de actualización de contraseña, por ejemplo, debido a que el token
+    | o la nueva contraseña no son válidos.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor, espera antes de volver a intentarlo.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+
+];
